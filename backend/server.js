ada1d58992189fe1ed99b36e9054f3d154622395
Joseph Jack <joseph.w.jack@gmail.com>
2023-09-21 12:33:06 -0700

    Setup user email & password validation

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ connectDB(); //Connect to MongoDB
 
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
