300f232a6b30c9d3503e024711726c44acbff697
Joseph Jack <joseph.w.jack@gmail.com>
2023-09-27 18:21:23 -0700

    Add template for login screen without functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,6 +11,7 @@ import store from "./store";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 import App from "./App";
 import "./assets/styles/bootstrap.custom.css";
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart/' element={<CartScreen />} />
+      <Route path='/login/' element={<LoginScreen />} />
     </Route>
   )
 );
