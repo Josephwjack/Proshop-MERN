c6c2ecddfba45f7722653ee81ba6da60b0b81ddb
Joseph Jack <joseph.w.jack@gmail.com>
2023-10-20 18:30:45 -0700

    Add: Ability to create new products as admin role

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getProductById } from "../controllers/productController.js";
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id").get(getProductById);
 
 export default router;
