4d6e36fa359bb88d7cb60de51641c3ffdcf32f3b
Joseph Jack <joseph.w.jack@gmail.com>
2023-11-17 13:37:43 -0800

    Add search box component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Paginate from "../components/Paginate";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
@@ -7,12 +8,13 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber});
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber});
 
   return (
     <>
+    { keyword && <Link to='/' className='btn btn-light mb-4'>Go Back</Link> }
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -32,6 +34,7 @@ const HomeScreen = () => {
           <Paginate
             pages={data.pages}
             page={data.page}
+            keyword={keyword ? keyword : ""}
             />
         </>
       )}
